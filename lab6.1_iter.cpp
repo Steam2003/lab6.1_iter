20cbf3482dde0ea45717b35b2b0cbc5da1aec74b
Ivan Patiuk <gendalf2003@gmail.com>
2020-11-12 12:56:27 +0200

    lab6.1_iter_fix

--- lab6.1_iter.cpp
+++ lab6.1_iter.cpp
@@ -3,48 +3,13 @@
 #include <iomanip>
 
 using namespace std;
-void iter(int* mas, int* mas2)
-{
-	int j = 0;
-	int sum = 0;
-	int Low = -24;
-	int Max = 32;
-	{
-		for (int i = 0; i <= 19; ++i)
-		{
-			mas[i] = Low + rand() % (Max - Low + 1);
-			if (mas[i] < 0 && mas[i] % 2 == 0)
-			{
-				mas2[i] = 0;
-				sum += mas[i];
-				++j;
-			}
-			else
-				mas2[i] = mas[i];
-			cout << setw(3) << mas[i] << " ";
-		}
-		cout << endl;
-		cout << "________________________________________________________________________________" << endl;
-		cout << "Ñóììà òèõ, ÿê³ º ïàðí³ òà â³ä'ºìí³: " << sum << endl;
-		cout << "Ê³ëüê³ñòü òèõ, ÿê³ º ïàðí³ òà â³ä'ºìí³: " << j << endl;
-		cout << "________________________________________________________________________________" << endl;
 
-	}
-}
-void print(int* mas2)
-{
-	for (int i = 0; i <= 19; ++i)
-		cout << setw(3) << mas2[i] << " ";
-}
-int unittest(int a[], int i, int sum)
-{
-	for (i; i <= 9; ++i)
-	{
-		if (a[i] < 0 && a[i] % 2 == 0)
-			sum += a[i];
-	}
-	return sum;
-}
+void create(int* mas);
+void create2(int* mas, int* mas2);
+void print(int* mas);
+int sum1(int* mas);
+int count1(int* mas);
+int unittest(int a[], int i, int sum);
 
 int main()
 {
@@ -54,13 +19,72 @@ int main()
 	int mas[20];
 	cout << "²òåðàö³éíèé" << endl;
 	int* mas2 = new int[20];
-	iter(mas, mas2);
+	create(mas);
+	print(mas);
+	cout << endl;
+	create2(mas, mas2);
 	print(mas2);
+	cout << endl;
+	cout << "Ñóììà òèõ, ÿê³ º ïàðí³ òà â³ä'ºìí³: " << sum1(mas) << endl;
+	cout << "Ñóììà òèõ, ÿê³ º ïàðí³ òà â³ä'ºìí³: " << count1(mas) << endl;
 	delete[] mas2;
 	int a[10] = { -5,6,10,-14,0,13,16,-10,20,32 };
 	cout << endl;
 	cout << "Unit test result: " << unittest(a, 0, 0) << endl;
 	cout << "________________________________________________________________________________" << endl;
 	
+}
+void create(int* mas)
+{
+	int Low = -24;
+	int Max = 32;
+		for (int i = 0; i <= 19; ++i)
+			mas[i] = Low + rand() % (Max - Low + 1);
+}
+void create2(int* mas, int* mas2)
+{
+	for (int i = 0; i <= 19; ++i)
+	{
+		if (mas[i] < 0 && mas[i] % 2 == 0)
+		{
+			mas2[i] = 0;
+		}
+		else
+			mas2[i] = mas[i];
+	}
+}
+void print(int* mas)
+{
+	for (int i = 0; i <= 19; ++i)
+		cout << setw(3) << mas[i] << " ";
+}
+int sum1(int* mas)
+{
+	int sum = 0;
+	for (int i = 0; i<=19;++i)
+	{
+		if(mas[i] < 0 && mas[i] % 2 == 0)
+		sum += mas[i];
+	}
+	return sum;
+}
+int count1(int* mas)
+{
+	int j = 0;
+	for (int i = 0; i <= 19; ++i)
+	{
+		if (mas[i] < 0 && mas[i] % 2 == 0)
+			++j;
+	}
+	return j;
+}
+int unittest(int a[], int i, int sum)
+{
+	for (i; i <= 9; ++i)
+	{
+		if (a[i] < 0 && a[i] % 2 == 0)
+			sum += a[i];
+	}
+	return sum;
 }
 
